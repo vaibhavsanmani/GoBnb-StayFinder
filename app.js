eb01766346a6021744138c0cea72431718309219
vaibhav sanmani <vaibhavsanmani9@gmail.com>
2026-01-28 23:41:58 +0530

    added delete review functionality also styled it properly

--- app.js
+++ app.js
@@ -150,6 +150,13 @@ app.post("/listings/:id/reviews", validateReview,wrapAsync(async (req, res) => {
     res.redirect(`/listings/${listing._id}`);
 }));
 
+//delete review
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=> {
+    let {id,reviewId}=req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+}))
 
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found"));
